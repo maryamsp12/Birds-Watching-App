a9bc1a04fb7a3653b25fa46c31ecd7b52395df5a
maryamsp12 <maryamsp12@example.com>
2019-02-08 23:44:08 +0200

    Updates

---
+++ precache-manifest.e321e0a049466e1b2b8468b3f62a42f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4264bf09daaeeb77a19",
+    "url": "/birds-watching-app/static/js/runtime~main.a4264bf0.js"
+  },
+  {
+    "revision": "eb6a611991c746deefbf",
+    "url": "/birds-watching-app/static/js/main.eb6a6119.chunk.js"
+  },
+  {
+    "revision": "650c378ad5b91c58c785",
+    "url": "/birds-watching-app/static/js/1.650c378a.chunk.js"
+  },
+  {
+    "revision": "eb6a611991c746deefbf",
+    "url": "/birds-watching-app/static/css/main.db5a57b2.chunk.css"
+  },
+  {
+    "revision": "4d5b46f3e604389756efe6d34a4ae2eb",
+    "url": "/birds-watching-app/index.html"
+  }
+];
