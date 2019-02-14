d9c8571c298e76929719386049d2da15bb7d794e
Maryam Tufail <maryamsp12@gmail.com>
2019-02-14 13:15:24 +0200

    Update homePage.jsx

--- src/homePage.jsx
+++ src/homePage.jsx
@@ -2,20 +2,25 @@ import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 
 
-
 class Home extends Component {
     state = {}
      render() { 
         
         return ( 
-            <React.Fragment>
+            <div className="bg">
                 <div className="App">
                     <header className="App-header">
-                    <h1 className="App-title">Birds Watching Application</h1>
-                    <button><Link to="/form">Add New Observation</Link></button>
+                    <h1 className="App-title">Birds Watching <br/>Application</h1>
+                    <button className='button'><Link to="/form">Add New Observation</Link></button>
                     </header>
+                <div className='container'>
+
+                
+
                 </div>
-            </React.Fragment>
+                   
+                </div>
+            </div>
          );
     }
 }
